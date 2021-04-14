f0cb391432ff8ec641aeff0297c3174c12f0f587
Jacob Butters <jacobbutters@gmail.com>
2021-04-14 14:53:50 -0600

    project setup

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { useInterval } from './useInterval';
+import { CANVAS_SIZE, SNAKE_START, APPLE_START, SCALE, SPEED, DIRECTIONS } from './constants';
+
+
+
+const App = () => {
+
+  const startGame = () => { }
+
+  const endGame = () => { }
+
+  const moveSnake = () => { }
+
+  const createApple = () => { }
+
+  return (
+    <div>Snake Game</div>
+  )
+}
+
+export default App;
